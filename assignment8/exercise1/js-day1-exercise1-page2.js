d051711bf30948799428bf884fa93979d0eb5cfe
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-04-26 16:15:14 +0200

    Small correction

--- assignment8/exercise1/js-day1-exercise1-page2.js
+++ assignment8/exercise1/js-day1-exercise1-page2.js
@@ -24,7 +24,7 @@ document.getElementById('div22').addEventListener('click', function(event) {
 
 
 
-// 1.4.4 and 1.4.6. See also js-exercise1-page2.html
+// 1.4.4, 1.4.5, 1.4.6. See also js-exercise1-page2.html
 
 document.getElementById('outer').addEventListener('click', function(event) {
 
@@ -33,51 +33,7 @@ document.getElementById('outer').addEventListener('click', function(event) {
     document.getElementById('output').innerText = 'Hi from ' + divId;
     console.log("Hi from outer")});
 
-
-// 1.4.5
-
-
-document.getElementById('div32').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div42').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div52').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div62').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div72').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div82').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div92').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div102').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div112').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-document.getElementById('div122').addEventListener('click', function(event) {
-    const divId = event.target.id;
-    console.log("Hi from " + divId)});
-
-
+    
 
 //###################################################################
 // 1.5
