e6c7e63f17df21e97d31d7ba1e8518b8383eb4ec
TRossen89 <110735176+TRossen89@users.noreply.github.com>
2024-05-02 02:47:41 +0200

    tick counter works but not clean

--- assignment9/react-tick-counter-exercise/tickCounterApp/src/TickCounter.jsx
+++ assignment9/react-tick-counter-exercise/tickCounterApp/src/TickCounter.jsx
@@ -1,53 +1,87 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 
-function startTimer(){
-    
-}
-function resetTimer(){
 
-}
 function TickCounter (){
 
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
-    const [startStop, setStartStop] = useState(null);
+    const [start, setStart] = useState(false);
+
+    const resetTimer = () => {
+      setStart(false);
+      setMinutes(0);
+      setSeconds(0);
+
+    }
+
+    const startTimer = () => {
+      setStart(true);
+    }
+
+    useEffect(() => {
+      
+      if(start){
+
+
+        const timer = () => {
+
+          if(minutes == 0 && seconds == 0){
+            setStart(false)
+            setMinutes(0)
+            setSeconds(0)
+          } else
+
+          {
+            if(seconds > 0){
+              setSeconds((seconds) => seconds - 1)
+            }
+            if(seconds == 0){
+    
+              setMinutes((minutes) => minutes -1)
+              setSeconds(59)
+            } 
+          }
+        
+      }
+    
+    const intervalId = setInterval(timer, 1000);
+
+    return () => clearInterval(intervalId) 
+      }
+  
+  }, [start, seconds]);
 
     return (
         <div>
         
-    
 
           <div>
             <input
               type="number"
               placeholder="Minutes"
               //value={minutes}
-              //onChange={(e) => setMinutes(parseInt(e.target.value))}
+              onChange={(e) => setMinutes(parseInt(e.target.value))}
             />
             <input
               type="number"
               placeholder="Seconds"
               //value={seconds}
-              //onChange={(e) => setSeconds(parseInt(e.target.value))}
+              onChange={(e) => setSeconds(parseInt(e.target.value))}
             />
             <button onClick={startTimer}>Start</button>
             <button onClick={resetTimer}>Reset</button>
           </div>
-          <div>Time Remaining: {//formatTime(time)
-          }
+          <div>Time Remaining: 
+            <div id= "clockDiv">
+              <div class="secondsAndMinutes" >{minutes}</div>
+              <div class="secondsAndMinutes" >{seconds}</div>
+            </div>
           </div>
         </div>
       );
 }
 
-function formatTime(time){
-
-    return (<div id= "clockDiv">
-    <div class="secondsAndMinutes">{minutes}</div>
-    <div class="secondsAndMinutes">{seconds}</div>
-</div>)
-}
 
 export default TickCounter;
